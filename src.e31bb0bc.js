fb113f36f946418270974473c0c7bca1ebcde768
Petr.Bur <gtnh48965@bk.ru>
2023-02-20 19:26:17 +0300

    d

--- src.e31bb0bc.js
+++ src.e31bb0bc.js
@@ -31102,7 +31102,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "59047" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51724" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
